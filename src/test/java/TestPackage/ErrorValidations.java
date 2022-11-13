f545cb6674196b0dbdea9e56d04e2aa51bea57a7
farukayaz <frkyz07.13@gmail.com>
2022-11-13 17:30:31 +0300

    - testing.xml file added

--- src/test/java/TestPackage/ErrorValidations.java
+++ src/test/java/TestPackage/ErrorValidations.java
@@ -1,5 +1,6 @@
 package TestPackage;
 
+import PageObjects.CartsPage;
 import TestComponents.BaseTest;
 import PageObjects.ProductPage;
 import org.testng.Assert;
@@ -9,15 +10,24 @@ import java.io.IOException;
 
 public class ErrorValidations extends BaseTest {
 
+    String productName = "ADIDAS ORIGINAL";
     @Test
-    public void submitOrder() throws IOException {
-
-        String productName = "ADIDAS ORIGINAL";
-        // TESTING WITH PAGE OBJECT MODEL
+    public void loginError() throws IOException {
 
         landingPage.loginApplication("faruk@ayasdaz.com", "Fasdaruk.1313");
         Assert.assertEquals("Incorrect email or password.",landingPage.getErrorMessage());
 
     }
+    @Test
+    public void productNameError() {
+        ProductPage productPage = landingPage.loginApplication("faruk@ayaz.com", "Faruk.1313");
+        CartsPage cartsPage = productPage.addProductToCard(productName);
+
+        String wrongProductName = "something";
+        Assert.assertFalse(cartsPage.goToCartsPageAndCheckTheOrder(wrongProductName));
+
+    }
+
+
 
 }
